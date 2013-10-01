cb3fe38ae0582c98a7a298b260560fee95590f92
DzikuVx <pawel@spychalski.info>
2013-10-01 19:48:38 +0200

    some code

---
+++ piPlayer.py
@@ -0,0 +1,32 @@
+import smbus
+import time
+
+# define I2C bus number
+BUS_NUMBER = 1
+
+# define device address
+DEVICE_ADDR = 0x20
+
+bus = smbus.SMBus(BUS_NUMBER)
+
+# PULLUP all ports to enable button state readout
+writeVal = 255
+bus.write_byte(DEVICE_ADDR,writeVal)
+
+while 1==1:
+        #get current value from register
+        currentVal = bus.read_byte(DEVICE_ADDR)
+
+        #shift input bits (0-3) to output position (4-7) by shifting them left $
+        writeVal = currentVal << 4
+
+        #set input bits (0-3) to 1 to archive PULLUP
+        writeVal = writeVal + 15
+
+        #get only first 8 bits by doing modulo, this step can be ommited
+        writeVal = writeVal % 256
+
+        #write to register
+        bus.write_byte(DEVICE_ADDR,writeVal)
+
+        time.sleep(0.1)
